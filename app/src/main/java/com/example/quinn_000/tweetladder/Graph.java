a4d2aa27148f2a54017bfd0f81e3e560c639afc4
Quinn Z <Quinn Z>
2016-10-23 06:40:13 -0500

    initial commit

---
+++ app/src/main/java/com/example/quinn_000/tweetladder/Graph.java
@@ -0,0 +1,122 @@
+package com.example.quinn_000.tweetladder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import java.util.ArrayList;
+
+import android.widget.TextView;
+
+
+/**
+ * Created by Hamza on 10/22/16.
+ */
+
+
+
+class Graph extends AppCompatActivity {
+
+    BarChart barChart;
+    /*
+    public int root = 100;
+    public int firstLayer = 50;
+    public int secondLayer = 10;
+    public int thirdLayer = 40;
+    */
+/*
+    private void kill_activity()
+    {
+
+        finish();
+    }
+*/
+    @Override
+    public void onBackPressed() {
+        finish();
+        super.onBackPressed();
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_graphs);
+
+
+        Bundle b = getIntent().getExtras();
+
+        int val1 = b.getInt("D1");
+        int val2 = b.getInt("D2");
+        int val3 = b.getInt("D3");
+        int val4 = b.getInt("D4");
+
+        String usernameV = b.getString("username");
+        String keywordV = b.getString("keyword");
+
+
+        TextView sumBoxField = (TextView) findViewById(R.id.sumBox);
+        sumBoxField.setText(usernameV + "-" + keywordV + " Relevance");
+
+
+
+        barChart = (BarChart) findViewById(R.id.action_bargraph);
+        ArrayList<BarEntry> barEntries = new ArrayList<>();
+        barEntries.add(new BarEntry(0f, val1));
+        barEntries.add(new BarEntry(1f, val2));
+      //  barEntries.add(new BarEntry(2f, val3));
+        //barEntries.add(new BarEntry(3f, val4));
+
+        /*Y axis*/
+        YAxis yAxis = barChart.getAxisRight();
+        YAxis leftAxis = barChart.getAxisLeft();
+
+        barChart.getAxisRight().setEnabled(false);
+        XAxis xAxis = barChart.getXAxis();
+        barChart.getXAxis().setEnabled(false);
+        barChart.getAxisRight().setEnabled(false);
+        BarDataSet barDataSet = new BarDataSet(barEntries, "");
+        /* sets color*/
+
+        barDataSet.setColor(0xFF4500, 180);
+
+
+
+        //disable excess stuff
+        barChart.getLegend().setEnabled(false);
+        barChart.getDescription().setEnabled(false);
+
+        BarData theData = new BarData(barDataSet);
+
+
+
+        //set width of bar
+        theData.setBarWidth(0.7f);
+
+
+        //aniamte in y and x direction
+        barChart.animateY(2000, Easing.EasingOption.EaseInQuart);
+        barChart.animateX(2000);
+        //barChart.animateXY(450, 3500);
+        barChart.setData(theData);
+
+        //input stuff
+        barChart.setTouchEnabled(true);
+        barChart.setDragEnabled(true);
+        barChart.setScaleEnabled(true);
+
+
+    }
+
+
+
+
+
+
+}
---
+++ app/src/main/java/com/example/quinn_000/tweetladder/MainActivity.java
@@ -0,0 +1,125 @@
+package com.example.quinn_000.tweetladder;
+
+import android.os.Handler;
+
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.view.View;
+
+import java.lang.Thread;
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static String usernameString;
+    private static String keywordString;
+
+    public Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            if(msg == null){
+                return;
+            }
+            ProgressBar p = (ProgressBar) findViewById(R.id.progressBar);
+            p.setVisibility(View.INVISIBLE);
+            goToGraph();
+        }
+
+    };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        EditText t1 = (EditText) findViewById(R.id.usernameBox);
+        EditText t2 = (EditText) findViewById(R.id.keywordBox);
+
+        t1.setEnabled(true);
+        t2.setEnabled(true);
+
+        t1.setText("");
+        t2.setText("");
+
+
+        Button b = (Button) findViewById(R.id.button);
+        b.setVisibility(View.VISIBLE);
+
+    }
+
+    public void goToGraph(){
+        Intent i = new Intent(this, Graph.class);
+
+        i.putExtra("D1", 72);
+        i.putExtra("D2", 89);
+        i.putExtra("D3", 5);
+        i.putExtra("D4", 15);
+
+        i.putExtra("username", usernameString);
+        i.putExtra("keyword", keywordString);
+        startActivity(i);
+        return;
+    }
+
+    public void buttonClick(View view) {
+
+        EditText t1 = (EditText) findViewById(R.id.usernameBox);
+        EditText t2 = (EditText) findViewById(R.id.keywordBox);
+
+        String username = (String) t1.getText().toString();
+        String keyword = (String) t2.getText().toString();
+        if(username.equals("") || keyword.equals("")){
+            return;
+        }
+
+        Runnable runnable = new Runnable() {
+            String username;
+            String keyword;
+            public void runnable (String username, String keyword){
+                this.username = username;
+                this.keyword = keyword;
+            }
+
+            @Override
+            public void run() {
+                try {
+                    System.out.println(username);
+                    Thread.sleep(4000);
+                } catch (InterruptedException e) {}
+                handler.sendEmptyMessage(0);
+            }
+        };
+
+
+        ProgressBar p = (ProgressBar) findViewById(R.id.progressBar);
+        p.setVisibility(View.VISIBLE);
+
+        Thread t = new Thread(runnable);
+
+
+        usernameString = username;
+        keywordString = keyword;
+
+        System.out.println(username + keyword);
+
+        Button b = (Button) findViewById(R.id.button);
+        b.setVisibility(View.GONE);
+
+        t1.setEnabled(false);
+        t2.setEnabled(false);
+
+
+        t.start();
+
+        return;
+    }
+}
